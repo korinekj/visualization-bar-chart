05cb4c54fa96b430c234388575cbb557cc3c3c96
Jarda Korinek <korinek14.j@gmail.com>
2022-07-28 00:06:27 +0200

    loaded external data from json using d3, then trying to visualize it...

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-console.log("From index.js");
+// const xhr = new XMLHttpRequest();
+// xhr.open(
+//   "GET",
+//   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+//   true
+// );
+// xhr.send();
+// xhr.onload = function () {
+//   const json = JSON.parse(xhr.responseText);
+//   insertInHeading(json);
+//   insertInParagraph(json);
+// };
 
-d3.select("h1").style("color", "blue");
+// fetch(
+//   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     insertInHeading(data);
+//   });
+d3.json(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+).then(function (data) {
+  console.log(data);
+  const svgWidth = 500,
+    svgHeight = 300;
 
-function insertInHeading(json) {
-  document.getElementById("nadpis").innerHTML = JSON.stringify(json);
-}
+  const barWidth = svgWidth / data.data.length;
 
-function insertInParagraph(json) {
-  document.getElementById("paragraph").innerHTML = JSON.stringify(json);
-}
+  const svg = d3
+    .select("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-const xhr = new XMLHttpRequest();
-xhr.open(
-  "GET",
-  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
-  true
-);
-xhr.send();
-xhr.onload = function () {
-  const json = JSON.parse(xhr.responseText);
-  insertInHeading(json);
-  insertInParagraph(json);
-};
+  console.log(data.data);
+
+  svg
+    .selectAll("rect")
+    .data(data.data)
+    .enter()
+    .append("rect")
+    .attr("y", function (d) {
+      return svgHeight - d[1] / 10;
+    })
+    .attr("height", function (d) {
+      return d[1];
+    })
+    .attr("width", barWidth)
+    .attr("transform", function (d, i) {
+      let translate = [barWidth * i, 0];
+      return "translate(" + translate + ")";
+    });
+});
